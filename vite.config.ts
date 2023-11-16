4280e99c296322691f26c686419d13174636a929
gomao <gboycdw@gmail.com>
2023-11-16 15:25:59 +0900

    chore: .env사용을 위한 기본 패키지 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import envCompatiblePlugin from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), envCompatiblePlugin(/* options */)],
+  define: {
+    "process.env": process.env, // process.env를 전달
+  },
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@containers": path.resolve(__dirname, "./src/containers"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@atoms": path.resolve(__dirname, "./src/atoms"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+      "@tools": path.resolve(__dirname, "./src/tools"),
     },
   },
   css: {
