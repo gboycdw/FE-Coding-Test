58cd76bad191f1068e7ea2c3c0ec74ef2e2ebd2a
gomao <gboycdw@gmail.com>
2023-11-16 15:29:51 +0900

    feat: env 설정 확인 및 gitignore 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 export default function App() {
+  const env = process.env.VITE_MY_KEY;
   return (
     <div className="text-gomao bg-green-50" style={{ padding: 15 }}>
-      my project
+      my project : {env} from env
     </div>
   );
 }
