675ee3a2dec43ba3b38f62e02af8b6aedf4ed9d4
gomao <gboycdw@gmail.com>
2024-08-05 03:38:47 +0900

    refactor: code fomatting

--- src/container/CatViewer/skeletonImages.tsx
+++ src/container/CatViewer/skeletonImages.tsx
@@ -1,6 +1,6 @@
 export default function SkeletonImages() {
   return (
-    <div className="animate-pulse flex flex-col items-center space-y-4 p-4 border rounded shadow-lg bg-gray-300 ">
+    <div className={`animate-pulse flex flex-col items-center space-y-4 p-4 border rounded shadow-lg bg-gray-300`}>
       <div className="h-60 w-full mb-4"></div>
     </div>
   );
