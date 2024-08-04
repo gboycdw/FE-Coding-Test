6116bd813b8a195b29d87407972b9d760cdf06cd
gomao <gboycdw@gmail.com>
2024-08-04 16:26:11 +0900

    feat: .env 및 api key 설정 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const URL = 'https://api.thecatapi.com/v1/images';
+const URL = "https://api.thecatapi.com/v1/images";
 
 const config = {
-  'Content-Type': 'application/json',
-  'x-api-key': process.env.REACT_APP_API_KEY_1,
+  "Content-Type": "application/json",
+  "x-api-key": process.env.VITE_API_KEY_1,
 };
 
 export const api_ = axios.create({
