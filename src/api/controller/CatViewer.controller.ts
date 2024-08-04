10a2e6576f0b1a4de1dbb05690aedaef63546282
gomao <gboycdw@gmail.com>
2024-08-05 04:01:30 +0900

    feat: 이미지 패칭함수의 에러 핸들러 추가

--- src/api/controller/CatViewer.controller.ts
+++ src/api/controller/CatViewer.controller.ts
@@ -1,5 +1,9 @@
 import { api_ } from "@api";
 
 export const getCatImages = async ({ limit, page }: { limit: number; page?: number }) => {
-  return await api_.get(`/search?limit=${limit}${page ? `&page=${page}` : ""}`);
+  try {
+    return await api_.get(`/search?limit=${limit}${page ? `&page=${page}` : ""}`);
+  } catch (e) {
+    throw new Error("이미지 로딩에 실패하였습니다");
+  }
 };
