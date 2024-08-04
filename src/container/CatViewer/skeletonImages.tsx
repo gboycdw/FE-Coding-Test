87882290c7873ab9044a041c4f5e92109f53e5bd
gomao <gboycdw@gmail.com>
2024-08-05 02:57:06 +0900

    feat: 스켈레톤 UI 생성

---
+++ src/container/CatViewer/skeletonImages.tsx
@@ -0,0 +1,7 @@
+export default function SkeletonImages() {
+  return (
+    <div className="animate-pulse flex flex-col items-center space-y-4 p-4 border rounded shadow-lg bg-gray-300 ">
+      <div className="h-60 w-full mb-4"></div>
+    </div>
+  );
+}
