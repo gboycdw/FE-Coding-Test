199fd66dd8dc20a48ef983e25fc08dd966fadfd4
gomao <gboycdw@gmail.com>
2024-08-05 03:38:18 +0900

    style: 부모 컴포넌트의 width 를 100%로 확장

--- src/pages/CatViewerPage.tsx
+++ src/pages/CatViewerPage.tsx
@@ -2,7 +2,7 @@ import CatViewer from "../container/CatViewer";
 
 export default function CatViewerPage() {
   return (
-    <div className="max-w-[1200px]">
+    <div className="w-full max-w-[1200px]">
       <CatViewer />
     </div>
   );
