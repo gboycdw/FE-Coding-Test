feb79763807e15d8d572b49bffc1ca1511c899ba
goamo <gboycdw@gmail.com>
2024-08-05 18:42:08 +0900

    feat: workingHour 페이지 초기 상태 구현 및 버튼 출력 로직 작성

--- src/container/WorkingHour/index.tsx
+++ src/container/WorkingHour/index.tsx
@@ -1,14 +1,31 @@
+import { DEFAULT_WORKING_HOUR_TABLE, RangeInputDataType, weekDay, workingHourState } from "@atoms";
 import RangeInput from "@components/common/rangeInput";
-import SelectorInput, { TimeOption } from "@components/common/selectorInput";
-import { useState } from "react";
+import { useRecoilValue } from "recoil";
 
 export default function WorkingHour() {
-  const [selectedTimeEnd, setSelectedTimeEnd] = useState<TimeOption>("00:00");
-
+  const workingHourTable = useRecoilValue(workingHourState);
   return (
-    <div>
-      <SelectorInput selectedTime={selectedTimeEnd} setSelectedTime={setSelectedTimeEnd} />
-      {/* <RangeInput /> */}
+    <div className="flex flex-col gap-3">
+      {Object.keys(DEFAULT_WORKING_HOUR_TABLE).map((day, weekIndex) => {
+        return (
+          <div className="flex gap-3 items-center pb-3 border-b" key={weekIndex}>
+            <div className="w-[120px]">{day}</div>
+
+            {workingHourTable[day as weekDay].map((table: RangeInputDataType, index: number) => {
+              const rangeInputCount = workingHourTable[day as weekDay].length;
+              return (
+                <div key={index} className="flex gap-2">
+                  <RangeInput day={day as weekDay} index={table.index} />
+                  <div className="flex items-center gap-4">
+                    {rangeInputCount > 0 && <div>X</div>}
+                    {index === rangeInputCount - 1 && <div>➕</div>}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
