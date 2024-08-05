b17070036f0f50178e43dbcca63dc84bdef4b94a
goamo <gboycdw@gmail.com>
2024-08-05 18:23:11 +0900

    feat: rangeInput 컴포넌트 생성

--- src/components/common/rangeInput.tsx
+++ src/components/common/rangeInput.tsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import SelectorInput, { TimeOption } from "./selectorInput";
+
 export default function RangeInput() {
-  return <div>rangeInput</div>;
+  const [selectedTimeEnd, setSelectedTimeEnd] = useState<TimeOption>("00:00");
+  const [selectedTimeStart, setSelectedTimeStart] = useState<TimeOption>("00:00");
+  return (
+    <div>
+      <div className="flex gap-2 items-center">
+        <SelectorInput selectedTime={selectedTimeStart} setSelectedTime={setSelectedTimeStart} />
+        <div>-</div>
+        <SelectorInput selectedTime={selectedTimeEnd} setSelectedTime={setSelectedTimeEnd} />
+      </div>
+    </div>
+  );
 }
