bc3d74b0f584c068a5bbbba90c8bbd659b613548
gomao <gboycdw@gmail.com>
2024-08-06 07:51:15 +0900

    feat: [추가 기능] 전체 Clear 버튼 생성

--- src/pages/WorkingHourPage.tsx
+++ src/pages/WorkingHourPage.tsx
@@ -1,51 +1,72 @@
 import { useRecoilState } from "recoil";
 import WorkingHour from "../container/WorkingHour";
-import { savedWorkingHourState, workingHourState } from "@atoms";
+import { DEFAULT_WORKING_HOUR_TABLE, savedWorkingHourState } from "@atoms";
 import { useCustomAlertHook } from "../hooks/customAlert";
 import { useState } from "react";
+import { somethingChangeState } from "@atoms/states/somethingChanged.stats";
 
 export default function WorkingHourPage() {
-  const [workingHourTable, setWorkingHourTable] = useRecoilState(workingHourState);
   const [savedWorkingHourTable, setSavedWorkingHourTable] = useRecoilState(savedWorkingHourState);
+  const [workingHourTable, setWorkingHourTable] = useState(savedWorkingHourTable);
   const [viewToggle, setViewToggle] = useState(true);
   const { customInfoMessage } = useCustomAlertHook();
+  const [somethingChange, setSomethingChangeState] = useRecoilState(somethingChangeState);
+  const [resetKey, setResetKey] = useState(0);
+
   return (
     <div className="flex gap-5">
       <div className="font-semibold h-full w-[200px]">Working Hour</div>
       <div>
         <div className="font-semibold border-b w-[550px] pb-3 flex justify-between">
           <div>Set your weekly hours</div>
-          <div
-            className="text-gray-500 cursor-pointer"
-            onClick={() => {
-              setViewToggle(!viewToggle);
-            }}
-          >
-            {viewToggle ? "▼" : "▲"}
+          <div className="flex gap-2">
+            <div
+              className="cursor-pointer px-2 py-1 items-center"
+              onClick={() => {
+                localStorage.removeItem("recoil-persist");
+                setWorkingHourTable(DEFAULT_WORKING_HOUR_TABLE);
+                setSomethingChangeState(false);
+                setResetKey((prev) => prev + 1);
+              }}
+            >
+              Clear
+            </div>
+            <div
+              className="text-gray-500 cursor-pointer items-center flex"
+              onClick={() => {
+                setViewToggle(!viewToggle);
+              }}
+            >
+              {viewToggle ? "▼" : "▲"}
+            </div>
           </div>
         </div>
         {viewToggle && (
           <>
-            <WorkingHour />
-            <div className="border-t pt-5 flex justify-end gap-6 items-center">
-              <div
-                className="py-2 px-7 items-center flex cursor-pointer hover:bg-gray-200"
-                onClick={() => {
-                  setWorkingHourTable(savedWorkingHourTable);
-                }}
-              >
-                Cancel
+            <WorkingHour resetTrigger={resetKey} workingHourTable={workingHourTable} setWorkingHourTable={setWorkingHourTable} />
+            {somethingChange && (
+              <div className="border-t pt-5 flex justify-end gap-6 items-center">
+                <div
+                  className="py-2 px-7 items-center flex cursor-pointer hover:bg-gray-200"
+                  onClick={() => {
+                    setWorkingHourTable(savedWorkingHourTable);
+                    setResetKey((prev) => prev + 1);
+                  }}
+                >
+                  Cancel
+                </div>
+                <div
+                  className="text-white bg-blue-700 py-2 px-7 items-center flex cursor-pointer hover:bg-blue-500"
+                  onClick={() => {
+                    setSavedWorkingHourTable(workingHourTable);
+                    setResetKey((prev) => prev + 1);
+                    customInfoMessage("현재 상태가 저장되었습니다", 1);
+                  }}
+                >
+                  Update
+                </div>
               </div>
-              <div
-                className="text-white bg-blue-700 py-2 px-7 items-center flex cursor-pointer hover:bg-blue-500"
-                onClick={() => {
-                  setSavedWorkingHourTable(workingHourTable);
-                  customInfoMessage("현재 상태가 저장되었습니다", 1);
-                }}
-              >
-                Update
-              </div>
-            </div>
+            )}
           </>
         )}
       </div>
