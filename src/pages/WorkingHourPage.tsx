9d7634b6c18ba7faa1efc46d93ae8e4588ce0794
gomao <gboycdw@gmail.com>
2024-08-06 05:58:04 +0900

    feat: working hour 레이아웃 작성

--- src/pages/WorkingHourPage.tsx
+++ src/pages/WorkingHourPage.tsx
@@ -2,8 +2,15 @@ import WorkingHour from "../container/WorkingHour";
 
 export default function WorkingHourPage() {
   return (
-    <div>
-      <WorkingHour />
+    <div className="flex gap-5">
+      <div className="font-semibold h-full w-[200px]">Working Hour</div>
+      <div>
+        <div className="font-semibold border-b pb-3 flex justify-between">
+          <div>Set your weekly hours</div>
+          <div className="text-gray-500">▼</div>
+        </div>
+        <WorkingHour />
+      </div>
     </div>
   );
 }
