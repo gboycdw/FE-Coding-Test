2a4f577f9925e63febd444ac5bd4d53df346fd33
gomao <gboycdw@gmail.com>
2024-08-06 08:24:37 +0900

    feat: 종료 시간이 시작 시간보다 빠를 경우 사용자가 알 수 있도록 에러메시지 및 문구 추가

--- src/components/common/rangeInput.tsx
+++ src/components/common/rangeInput.tsx
@@ -2,7 +2,10 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import SelectorInput, { TimeOption } from "./selectorInput";
 import { weekDay, WorkingHourTableType } from "@atoms";
 import { useSetRecoilState } from "recoil";
-import { somethingChangeState } from "@atoms/states/somethingChanged.stats";
+import { somethingChangeState } from "@atoms/states/somethingChanged.state";
+import { useCustomAlertHook } from "../../hooks/customAlert";
+import { TimeValidator } from "@tools/timeValidator";
+import { timeValidatorState } from "@atoms/states/timeValidator.state";
 
 interface RangeInputProps {
   day: weekDay;
@@ -16,25 +19,42 @@ export default function RangeInput(props: RangeInputProps) {
 
   const initialStart = workingHourTable[day]?.[index]?.start || "00:00";
   const initialEnd = workingHourTable[day]?.[index]?.end || "00:00";
-
+  const { customWarningMessage } = useCustomAlertHook();
   const [selectedTimeStart, setSelectedTimeStart] = useState<TimeOption>(initialStart);
   const [selectedTimeEnd, setSelectedTimeEnd] = useState<TimeOption>(initialEnd);
   const setSomethingChangeState = useSetRecoilState(somethingChangeState);
+  const [timeWrong, setTimeWrong] = useState(false);
+  const setTimeValidator = useSetRecoilState(timeValidatorState);
 
   useEffect(() => {
     if (selectedTimeStart !== initialStart || selectedTimeEnd !== initialEnd) {
-      setWorkingHourTable((prev) => ({ ...prev, [day]: [...prev[day].map((item) => (item.index === index ? { index, start: selectedTimeStart, end: selectedTimeEnd } : item))] }));
       setSomethingChangeState(true);
+      if (TimeValidator(selectedTimeStart, selectedTimeEnd)) {
+        setTimeValidator((prev) => ({ ...prev, [day]: true }));
+        setTimeWrong(false);
+      } else {
+        setTimeWrong(true);
+        setTimeValidator((prev) => ({ ...prev, [day]: false }));
+        customWarningMessage(`[${day}] The end time cannot be the same as or earlier than the start time.`);
+      }
+
+      setWorkingHourTable((prev) => ({
+        ...prev,
+        [day]: [...prev[day].map((item) => (item.index === index ? { index, start: selectedTimeStart, end: selectedTimeEnd } : item))],
+      }));
+    } else {
+      setSomethingChangeState(false);
     }
   }, [selectedTimeStart, selectedTimeEnd]);
 
   return (
     <div>
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-2 items-center relative">
         <SelectorInput selectedTime={selectedTimeStart} setSelectedTime={setSelectedTimeStart} />
         <div>-</div>
         <SelectorInput selectedTime={selectedTimeEnd} setSelectedTime={setSelectedTimeEnd} />
       </div>
+      {timeWrong && <div className="absolute text-[11px] w-[343px] flex justify-start text-red-500">[Error] Please check the time conditions.</div>}
     </div>
   );
 }
